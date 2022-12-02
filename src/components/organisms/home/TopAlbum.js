716afeb3c5bf8c8b2ca41edc4a9d396369b5750e
nity-su <7anlyn@gmail.com>
2022-12-02 23:44:31 +0900

    add item card

--- src/components/organisms/home/TopAlbum.js
+++ src/components/organisms/home/TopAlbum.js
@@ -1,15 +1,25 @@
 import styled from "styled-components";
+import ItemCard from "@components/molecules/ItemCard";
 
 const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   width: 100%;
   height: 100vh;
   background-color: blueviolet;
 `;
 
+const ItemCardsBox = styled.div``;
+
 export default function TopAlbum() {
   return (
     <>
-      <Container />
+      <Container>
+        <ItemCardsBox>
+          <ItemCard />
+        </ItemCardsBox>
+      </Container>
     </>
   );
 }
