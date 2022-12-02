2d482282bdd0b3b2508047efb7bf747c14d625fe
jhnam2250 <rs225@naver.com>
2022-12-02 23:42:27 +0900

    IntroPage

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Tradeplace from "./pages/Tradeplace";
 
 export default function Router() {
   return (
@@ -9,6 +10,7 @@ export default function Router() {
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />}></Route>
+          <Route path="/tradeplace" element={<Tradeplace></Tradeplace>}></Route>
         </Route>
       </Routes>
     </BrowserRouter>
