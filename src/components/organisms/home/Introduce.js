0737f2bc86e161fdd9cc4f6bc051dab7a73f1c84
jhnam2250 <rs225@naver.com>
2022-12-04 00:13:02 +0900

    Introduce Page - 1204

--- src/components/organisms/home/Introduce.js
+++ src/components/organisms/home/Introduce.js
@@ -1,88 +1,140 @@
 import React from "react";
 import styled from "styled-components";
 import LogoImg from "../../assets/images/metamusic_test_logo.png";
+import TestImg from "../../assets/images/testimg.jpg";
+import UserProfile from "../../assets/images/user_profile.png";
+import SearchLens from "../../assets/images/searchlens.png";
+import Wallet from "../../assets/images/wallet.png";
+//import CommingSoonCarousel from "./Introduce_page/Carousel";
 
 const BackGroundBox = styled.div`
   width: 100%;
-  height: 867px;
+  height: 1200px;
   background-size: cover;
 `;
 
 const NaviBox = styled.div`
   width: 100%;
   height: 70px;
-  background-color: gray;
   display: flex;
   flex-direction: row;
   justify-content: space-around;
 `;
 
 const LogoBox = styled.div`
-  width: 90px;
-  height: 50px;
+  width: 200px;
+  height: 100px;
   background-image: url(${LogoImg});
   background-repeat: no-repeat;
   background-size: contain;
 `;
 
-const SearchBox = styled.div`
-  width: 340px;
-  height: 30px;
-  border-radius: 30;
-  background-color: white;
+const SearchBoxContainer = styled.div`
+  display: flex;
   flex-direction: row;
+  justify-content: center;
+`;
+
+const SearchBox = styled.div`
+  margin-top: 10px;
+  width: 340px;
+  height: 50px;
+  border-color: black;
+  border-width: solid;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: row;
+  justify-content: start;
+  background-color: whitesmoke;
 `;
 
 const LensBox = styled.div`
-  width: 17px;
-  height: 17px;
+  margin-top: 15px;
+  margin-left: 10px;
+  width: 20px;
+  height: 20px;
   display: flex;
   justify-content: center;
-  background-color: black;
+  background-image: url(${SearchLens});
 `;
 
 const LensTextBox = styled.div`
+  margin-top: 15px;
+  margin-left: 10px;
   width: 53px;
   height: 17px;
   display: flex;
   justify-content: center;
-  background-color: yellow;
   text-decoration-color: gray;
+  font-size: 20px;
 `;
 
 const MarketAllBox = styled.div`
+  margin-top: 25px;
   width: 100px;
   height: 24px;
+  font-size: 20px;
+  font-weight: bold;
   display: flex;
   justify-content: center;
 `;
 
 const QnABox = styled.div`
+  margin-top: 25px;
   width: 100px;
   height: 24px;
+  font-size: 20px;
+  font-weight: bold;
   display: flex;
   justify-content: center;
 `;
 
 const ProfileBox = styled.div`
+  margin-top: 25px;
   display: flex;
   justify-content: center;
   flex-direction: row;
 `;
 
 const ProfileImgBox = styled.div`
-  width: 33px;
-  height: 33px;
-  background-color: black;
+  width: 24px;
+  height: 24px;
+  background-image: url(${UserProfile});
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 const ProfileTextBox = styled.div`
   width: 100px;
   height: 24px;
+  font-size: 20px;
+  font-weight: bold;
   display: flex;
   justify-content: center;
-  background-color: yellow;
-  text-decoration-color: gray;
+`;
+
+const ConnectWalletContainer = styled.div`
+  margin-top: 25px;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+const ConnectWalletImg = styled.div`
+  width: 24px;
+  height: 24px;
+  background-image: url(${Wallet});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const ConnectWalletText = styled.div`
+  width: 170px;
+  height: 24px;
+  font-size: 20px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
 `;
 
 const MetaMusicIntroBox = styled.div`
@@ -92,12 +144,17 @@ const MetaMusicIntroBox = styled.div`
 `;
 
 const MetaMusicIntro = styled.div`
-  width: 893px;
-  height: 400px;
-  background-color: orange;
+  margin-top: 50px;
+  width: 800px;
+  height: 550px;
+  border-radius: 20px;
+  background-image: url(${TestImg});
+  background-repeat: no-repeat;
+  background-size: cover;
 `;
 
 const CommingSoonContainerBox = styled.div`
+  margin-top: 50px;
   display: flex;
   flex-direction: row;
   justify-content: center;
@@ -112,12 +169,15 @@ const CommingSoonContainer = styled.div`
   background-color: white;
 `;
 
-const CommingSoonTitle = styled.div``;
+const CommingSoonTitle = styled.div`
+  font-size: 36px;
+  font-weight: bold;
+`;
 
 const CommingSoonImgBox = styled.div`
+  margin-top: 20px;
   width: 950px;
   height: 350px;
-  background-color: blueviolet;
   display: flex;
   justify-content: space-around;
 `;
@@ -125,18 +185,21 @@ const CommingSoonImgBox = styled.div`
 const CommingSoonImg1 = styled.div`
   width: 300px;
   height: 300px;
+  border-radius: 20px;
   background-color: red;
 `;
 
 const CommingSoonImg2 = styled.div`
   width: 300px;
   height: 300px;
+  border-radius: 20px;
   background-color: green;
 `;
 
 const CommingSoonImg3 = styled.div`
   width: 300px;
   height: 300px;
+  border-radius: 20px;
   background-color: blue;
 `;
 
@@ -145,15 +208,21 @@ function Introduce() {
     <BackGroundBox>
       <NaviBox>
         <LogoBox></LogoBox>
-        <SearchBox>
-          <LensBox></LensBox> <LensTextBox>Search</LensTextBox>
-        </SearchBox>
+        <SearchBoxContainer>
+          <SearchBox>
+            <LensBox></LensBox> <LensTextBox>Search</LensTextBox>
+          </SearchBox>
+        </SearchBoxContainer>
         <MarketAllBox>Market All</MarketAllBox>
         <QnABox>Q&A</QnABox>
         <ProfileBox>
           <ProfileImgBox></ProfileImgBox>
           <ProfileTextBox>Profile</ProfileTextBox>
         </ProfileBox>
+        <ConnectWalletContainer>
+          <ConnectWalletImg></ConnectWalletImg>
+          <ConnectWalletText>Connect Wallet</ConnectWalletText>
+        </ConnectWalletContainer>
       </NaviBox>
       <MetaMusicIntroBox>
         <MetaMusicIntro></MetaMusicIntro>
---
+++ src/components/organisms/home/Introduce_page/Metamusic_Intro.js
@@ -0,0 +1,5 @@
+import { Application } from "@splinetool/runtime";
+
+const canvas = document.getElementById("canvas3d");
+const app = new Application(canvas);
+app.load("https://prod.spline.design/XUNLz0vzGaadO8t7/scene.splinecode");
