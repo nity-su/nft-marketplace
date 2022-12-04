4dd118707ed6de1b61b1cebfa5479bbe84b36395
nity-su <7anlyn@gmail.com>
2022-12-04 22:57:10 +0900

    :lipstick: Add CardsItems
    working...

---
+++ src/components/molecules/ItemCards.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import ItemCard from "./ItemCard";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: grid;
+  width: 950px;
+  height: 550px;
+  grid-template-columns: 1fr 1fr;
+`;
+
+const ItemCardsWrapper = styled.div`
+  grid-template-columns: 1fr;
+  grid-template-rows: repeat(5, 20%);
+  row-gap: 12px;
+`;
+
+export default function ItemCards() {
+  const first_array = [];
+  const second_array = [];
+
+  for (let i = 1; i <= 10; i++) {
+    if (i < 6) {
+      first_array.push(i);
+    } else {
+      second_array.push(i);
+    }
+  }
+
+  return (
+    <Container>
+      <ItemCardsWrapper>
+        {first_array.map((x, index) => {
+          return <ItemCard key={index} rank={x} />;
+        })}
+      </ItemCardsWrapper>
+      <ItemCardsWrapper>
+        {second_array.map((x, index) => {
+          return <ItemCard key={index} rank={x} />;
+        })}
+      </ItemCardsWrapper>
+    </Container>
+  );
+}
