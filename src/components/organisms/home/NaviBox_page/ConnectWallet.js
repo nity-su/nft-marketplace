c7fe69cf85da6ff026fb30936196c3cf51d46c88
jhnam2250 <rs225@naver.com>
2022-12-22 18:18:37 +0900

    Metamask Connection

---
+++ src/components/organisms/home/NaviBox_page/ConnectWallet.js
@@ -0,0 +1,68 @@
+import React from "react";
+import styled from "styled-components";
+import Wallet from "../../../assets/images/wallet.png";
+
+const Button = styled.div`
+  width: 100%;
+  height: 100%;
+  cursor: pointer;
+`;
+
+const ConnectWalletContainer = styled.div`
+  margin-top: 25px;
+  display: flex;
+  justify-content: center;
+  flex-direction: row;
+`;
+
+const ConnectWalletImg = styled.div`
+  width: 24px;
+  height: 24px;
+  background-image: url(${Wallet});
+  background-repeat: no-repeat;
+  background-size: cover;
+`;
+
+const ConnectWalletText = styled.div`
+  width: 170px;
+  height: 24px;
+  font-size: 20px;
+  font-weight: bold;
+  display: flex;
+  justify-content: center;
+`;
+
+export default function ConnectWallet() {
+  return (
+    <>
+      <ConnectWalletContainer>
+        <Button
+          onClick={() => {
+            if (typeof window.ethereum !== "undefined") {
+              const ethereum = window.ethereum;
+              ethereum
+                .request({ method: "eth_requestAccounts" })
+                .then(console.log);
+            }
+          }}
+        >
+          <ConnectWalletImg></ConnectWalletImg>
+        </Button>
+
+        <Button
+          onClick={() => {
+            if (typeof window.ethereum !== "undefined") {
+              const ethereum = window.ethereum;
+              ethereum
+                .request({ method: "eth_requestAccounts" })
+                .then(console.log);
+            }
+          }}
+        >
+          <ConnectWalletText>Connect Wallet</ConnectWalletText>
+        </Button>
+      </ConnectWalletContainer>
+      ;
+    </>
+  );
+}
