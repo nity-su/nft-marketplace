2baeefb034c960bbda15ae1c21f1b253159311d0
GodCorea <4628c@naver.com>
2022-12-25 00:18:43 +0900

    Css 파괴 및 수정

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import Footer from "@components/organisms/home/Footer";
 import Introduce from "@components/organisms/home/Introduce";
 import TopAlbum from "@components/organisms/home/TopAlbumTab";
 import Question from "@components/organisms/home/Question";
-import NaviBox from "@components/organisms/home/NaviBox";
+
 
 export default function Home() {
   return (
