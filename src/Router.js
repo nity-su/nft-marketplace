04079bd601226d8ef1eb4754d2a1741b1e4bd024
GodCorea <4628c@naver.com>
2022-12-28 00:16:00 +0900

    스크롤 방법

--- src/Router.js
+++ src/Router.js
@@ -4,6 +4,7 @@ import Tradeplace from "./components/organisms/tradeplace/Tradeplace";
 import AllCollection from "./pages/AllCollection";
 import NFTList from "./pages/NFTList";
 import NaviBox from "@components/organisms/home/NaviBox";
+import Main from "@components/Main/Main";
 
 export default function Router() {
   return (
@@ -14,6 +15,7 @@ export default function Router() {
           <Route path="/tradeplace" element={<Tradeplace />}></Route>
           <Route path="/allcollection" element={<AllCollection />}></Route>
           <Route path="/nftlist" element={<NFTList />}></Route>
+          <Route path="/main" element={<Main />}></Route>
           </Route>
       </Routes>
     </BrowserRouter>
